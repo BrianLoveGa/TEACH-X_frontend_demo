5f530f2fc43e8eb74aebbcf68cb3d03717101868
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-27 19:32:59 -0500

    dinner break

--- site.js
+++ site.js
@@ -1,3 +1,11 @@
 console.log("test");
 
+///// show  comments for a standard
 
+let first = document.getElementById("item1");
+first.addEventListener("click", showStuff());
+
+function showStuff() {
+  let commentsOne = document.getElementById("hide1");
+  commentsOne.style.display = block;
+}
