11836c7623cc7bc13a2bd2b33ae73e557687700c
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-27 19:39:31 -0500

    ok now it works now I can eat

--- site.js
+++ site.js
@@ -3,9 +3,14 @@ console.log("test");
 ///// show  comments for a standard
 
 let first = document.getElementById("item1");
-first.addEventListener("click", showStuff());
+first.addEventListener("click", showStuff);
 
 function showStuff() {
   let commentsOne = document.getElementById("hide1");
-  commentsOne.style.display = block;
+  commentsOne.style.display = "block";
+}
+
+function closeIt1() {
+  let commentsOne = document.getElementById("hide1");
+  commentsOne.style.display = "none";
 }
