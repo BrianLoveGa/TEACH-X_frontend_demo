6c578484a96179124ccfcfa6b17a821565140c0e
Brian Loveless <Brian.loveless1@yahoo.com>
2020-03-03 12:55:37 -0500

    progress

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Wanted a website for teachers that they can post relevant content and lesson pla
 Create this in react and then react Native to make it a website and app for phones.
 
 After enough posts are in existance machine learning to help predict and suggest to teachers based on who/what they have liked and their unique style.
+
+Kinda like reddit or Instagram. It would be a place teachers can share resources (link articles, videos, post handouts, word-finds, vocab sheets) based on the cirriculum requirements. They would be able to search by subject, grade level, type of materials, (videos, hand-outs, articles) save their favorites, vote as a group community which is great which is relevant, or that which is terrible. Power to the peopl. Knowledge for all.
--- site.js
+++ site.js
@@ -1,16 +1,53 @@
 console.log("test");
 
 ///// show  comments for a standard
+let commentsOne = document.getElementById("hide1");
+let commentsTwo = document.getElementById("hide2");
+let commentsThree = document.getElementById("hide3");
+let commentsFour = document.getElementById("hide4");
+let commentsFive = document.getElementById("hide5");
 
 let first = document.getElementById("item1");
 first.addEventListener("click", showStuff);
 
 function showStuff() {
-  let commentsOne = document.getElementById("hide1");
   commentsOne.style.display = "block";
 }
 
 function closeIt1() {
-  let commentsOne = document.getElementById("hide1");
   commentsOne.style.display = "none";
 }
+
+let second = document.getElementById("item2");
+second.addEventListener("click", showStuff2);
+
+function showStuff2() {
+  commentsTwo.style.display = "block";
+}
+
+function closeit2() {
+  commentsTwo.style.display = "none";
+}
+
+let third = document.getElementById("item3");
+third.addEventListener("click", showStuff3);
+
+function showStuff3{
+  commentsThree.style.display = 'block'
+}
+
+function closeIt3(){
+  commentsThree.style.display = 'none'
+}
+
+let four = document.getElementById("item4");
+four.addEventListener("click", showStuff4);
+
+function showStuff4() {
+  commentsFour.style.display = "block";
+}
+
+function closeIt4() {
+  commentsFour.style.display = "none";
+}
+Í
